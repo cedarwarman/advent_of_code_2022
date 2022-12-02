7c78ebd69d8f07ad002bac7f9c6c7f632268cff6
cedarwarman <cedardalewarman@gmail.com>
2022-12-02 11:56:04 -0700

    Finish first puzzle.

--- python/01.py
+++ python/01.py
@@ -4,3 +4,25 @@
 #
 # for line in requests.get("https://adventofcode.com/2022/day/1/input"):
 #     print(line)
+
+import pathlib
+
+# Reading in the file
+f = open(pathlib.Path(__file__).resolve().parents[1] / "puzzle_input" / "puzzle_input_01.txt")
+
+sum = 0
+sums = []
+
+for line in f:
+    print(line)
+    if line.strip():
+        sum = sum + int(line.strip())
+        print("Sum = ", sum)
+
+    else:
+        print("Found empty line, appending sum and resetting")
+        sums.append(sum)
+        sum = 0
+
+print("Max is: ", max(sums))
+
