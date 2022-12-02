21434c2b2ca638150758f9984b5fb7c48793a135
cedarwarman <cedardalewarman@gmail.com>
2022-12-02 11:30:04 -0700

    Add puzzle input.

---
+++ python/01.py
@@ -0,0 +1,6 @@
+# Leaving this here for the future, but I learned that I have to be logged in
+# to see the puzzle input, so I'll download it to a local file and read it in.
+# import requests
+#
+# for line in requests.get("https://adventofcode.com/2022/day/1/input"):
+#     print(line)
