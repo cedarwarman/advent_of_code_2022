bc5f27e963ce5337c70772187754176e9fbc699d
cedarwarman <cedardalewarman@gmail.com>
2022-12-05 13:16:01 -0700

    Finish part 2

--- python/05.py
+++ python/05.py
@@ -22,21 +22,14 @@ for line in f:
 # Going through the main loop that moves the things
 f.readline() # Blank line
 for line in f:
-    print(line)
     regex_results = re.findall("\s\d+", line)
     find_list = []
     for thing in regex_results:
         find_list.append(int(thing.strip()))
-    print(find_list)
 
     # Doing the move(s)
     for z in range(find_list[0]):
-        print(z)
-        print(stacks[find_list[2] - 1])
-        print(stacks[find_list[1] - 1])
         stacks[find_list[2] - 1].append(stacks[find_list[1] - 1].pop())
-        print(stacks[find_list[2] - 1])
-        print(stacks[find_list[1] - 1])
 
 # Getting the answer from the last item in each stack
 answer_string = ""
@@ -44,4 +37,46 @@ for stack in stacks:
     last_item = stack[-1]
     answer_string += last_item
 
-print("Answer is: ", answer_string)
+print("Answer to part 1 is: ", answer_string)
+
+# Part 2
+f.seek(0)
+
+# Reading in the first stacks (look for two newlines to stop)
+stacks = [[] for x in range(9)]
+
+for line in f:
+    if line[1] == '1':
+        break
+
+    # Making a list of values for each stack
+    y = 0
+    for n in range(1, 34, 4):
+        entry = line[n]
+        if entry != ' ':
+            stacks[y].insert(0, entry)
+        y += 1
+
+# Going through the main loop that moves the things
+f.readline() # Blank line
+for line in f:
+    regex_results = re.findall("\s\d+", line)
+    find_list = []
+    for thing in regex_results:
+        find_list.append(int(thing.strip()))
+
+    # Doing the move(s)
+    # First add the items to the destination list
+    stacks[find_list[2] - 1] = stacks[find_list[2] - 1] + stacks[find_list[1] - 1][-find_list[0]:]
+
+    # Next remove them from the source list
+    stacks[find_list[1] - 1] = stacks[find_list[1] - 1][:-find_list[0]]
+
+# Getting the answer from the last item in each stack
+answer_string = ""
+for stack in stacks:
+    last_item = stack[-1]
+    answer_string += last_item
+
+print("Answer to part 2 is: ", answer_string)
+
